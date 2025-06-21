c2fc69843b4ad452e26a3a2b650d0cfbf1509c02
Sumeet_Gond <sumitv9009@gmail.com>
2025-06-21 13:20:47 +0530

    fix bugs and refactor

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,14 +1,17 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.models.js";
-import { uploadOnCloudniary } from "../utils/Cloudinary.js";
+import {
+  uploadOnCloudniary,
+  deleteFromCouldinary,
+} from "../utils/Cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  const { fullName, username, email, password } = req.body;
+  const { fullname, username, email, password } = req.body;
   //validations
   if (
-    [].some((field) => {
+    [fullname, username, email, password].some((field) => {
       field?.trim() === "";
     })
   ) {
@@ -30,38 +33,69 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(409, "User with email or username Already Exists");
   }
 
-  const avatarLocalPath = req.files?.avatar[0].path;
-  const coverLocalPath = req.files?.coverImage[0].path;
+  const avatarLocalPath = req.files?.avatar?.[0]?.path;
+  const coverLocalPath = req.files?.coverImage?.[0]?.path;
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is missing");
   }
 
-  const avatar = await uploadOnCloudniary(avatarLocalPath);
-  let coverImage = "";
-  if (coverLocalPath) {
+  //   const avatar = await uploadOnCloudniary(avatarLocalPath);
+  //   let coverImage = "";
+  //   if (coverLocalPath) {
+  //     coverImage = await uploadOnCloudniary(coverLocalPath);
+  //   }
+
+  let avatar;
+  try {
+    avatar = await uploadOnCloudniary(avatarLocalPath);
+    console.log("Avatar uploaded successfully", avatar);
+  } catch (error) {
+    console.log("error while uploading avatar", error);
+    throw new ApiError(500, "Failed to upload avatar");
+  }
+  let coverImage;
+  try {
     coverImage = await uploadOnCloudniary(coverLocalPath);
+    console.log("coverImage uploaded successfully", coverImage);
+  } catch (error) {
+    console.log("error while uploading coverImage", error);
+    throw new ApiError(500, "Failed to upload coverImage");
   }
 
-  const user = await User.create({
-    fullName,
-    avatar: avatar.url,
-    coverImage: coverImage?.url || "",
-    email,
-    password,
-    username: username.toLowerCase(),
-  });
+  try {
+    const user = await User.create({
+      fullname,
+      avatar: avatar.url,
+      coverImage: coverImage?.url || "",
+      email,
+      password,
+      username: username.toLowerCase(),
+    });
 
-  const createdUser = await User.findById(user._id).select(
-    "-password -refreshToken"
-  );
-  if (!createdUser) {
-    throw new ApiError(500, "Something was wrong while registering user");
+    const createdUser = await User.findById(user._id).select(
+      "-password -refreshToken"
+    );
+    if (!createdUser) {
+      throw new ApiError(500, "Something was wrong while registering user");
+    }
+
+    return res
+      .status(201)
+      .json(new ApiResponse(200, createdUser, "User register Successfully"));
+  } catch (error) {
+    console.log("User creation Failed");
+    if (avatar) {
+      await deleteFromCouldinary(avatar.public_id);
+    }
+    if (coverImage) {
+      await deleteFromCouldinary(coverImage.public_id);
+    }
+    throw new ApiError(
+      500,
+      "Something was wrong while registering user and images where deleted"
+    );
   }
-
-  return res
-    .status(201)
-    .json(new ApiResponse(200, createdUser, "User register Successfully"));
 });
 
 export { registerUser };
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,5 +1,9 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
+import dotenv from "dotenv";
+
+// Configuration
+dotenv.config();
 
 // Configuration
 cloudinary.config({
@@ -24,4 +28,14 @@ const uploadOnCloudniary = async (localFilePath) => {
   }
 };
 
-export { uploadOnCloudniary };
+const deleteFromCouldinary = async (publicId) => {
+  try {
+    const result = await cloudinary.uploader.destroy(publicId);
+    console.log("Deleted from cloudinary. Public Id:", publicId);
+  } catch (error) {
+    console.log("Error while deleting file");
+    return null;
+  }
+};
+
+export { uploadOnCloudniary, deleteFromCouldinary };
